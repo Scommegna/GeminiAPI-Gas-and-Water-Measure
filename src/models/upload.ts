a21fc540d56fb709a861db74b59753ed8def9bd5
Lucas Scommegna <scommegnal@hotmail.com>
2024-08-28 08:55:21 -0300

    feat: upload model in mongo

---
+++ src/models/upload.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+type Base64Image = `data:image/${
+  | "png"
+  | "jpeg"
+  | "gif"
+  | string};base64,${string}`;
+
+type MeasureType = "Water" | "Gas";
+
+export interface Upload extends Document {
+  image: Base64Image;
+  customer_code: string;
+  measure_datetime: Date;
+  measure_type: MeasureType;
+}
+
+const UploadSchema = new Schema<Upload>({
+  image: { type: String, required: true },
+  customer_code: { type: String, required: true, unique: true },
+  measure_datetime: { type: Date, required: true },
+  measure_type: { type: String, required: true },
+});
+
+const UploadModel = mongoose.model<Upload>("Upload", UploadSchema);
+
+export default UploadModel;
