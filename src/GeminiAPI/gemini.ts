ce64783827be512c30a3805a12d40d951de06794
Lucas Scommegna <scommegnal@hotmail.com>
2024-08-29 19:47:16 -0300

    feat: gemini inital config

---
+++ src/GeminiAPI/gemini.ts
@@ -0,0 +1,14 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleAIFileManager } from "@google/generative-ai/server";
+
+import "dotenv/config";
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY || "");
+
+export const fileManager = new GoogleAIFileManager(
+  process.env.GEMINI_API_KEY || ""
+);
+
+export const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
