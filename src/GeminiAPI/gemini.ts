a49e3ce6bdf066a21246d4bd186a57238aced2e6
Lucas Scommegna <scommegnal@hotmail.com>
2024-08-30 16:33:00 -0300

    feat: more gemini updates

--- src/GeminiAPI/gemini.ts
+++ src/GeminiAPI/gemini.ts
@@ -1,14 +1,25 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GoogleAIFileManager } from "@google/generative-ai/server";
 
+import fs from "fs";
+
+import path from "path";
+
 import "dotenv/config";
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY || "");
 
-export const fileManager = new GoogleAIFileManager(
-  process.env.GEMINI_API_KEY || ""
-);
+const fileManager = new GoogleAIFileManager(process.env.GEMINI_API_KEY || "");
 
-export const model = genAI.getGenerativeModel({
+const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
+
+export async function getMeasure(base64String: string, measureType: string) {}
+
+function base64ToImage(base64String: string, filePath: string) {
+  const base64Data = base64String.replace(/^data:image\/\w+;base64,/, "");
+  const imageBuffer = Buffer.from(base64Data, "base64");
+
+  fs.writeFileSync(filePath, imageBuffer);
+}
