5a6a3354ca9a9d0321632e63a6eca6a7143d017e
Lucas Scommegna <scommegnal@hotmail.com>
2024-12-13 19:02:11 -0300

    more of proof of payment route

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -7,11 +7,18 @@ import { upload } from "../middlewares/multerMiddleware";
 import {
   createUpload,
   getListOfMeasures,
+  sendProofOfPayment,
 } from "../controllers/uploadControllers";
 
 const router = express.Router();
 
 router.post("/upload", isAuthenticated, upload.single("file"), createUpload);
 router.get("/list", isAuthenticated, getListOfMeasures);
+router.post(
+  "/proof",
+  isAuthenticated,
+  upload.single("file"),
+  sendProofOfPayment
+);
 
 export { router };
